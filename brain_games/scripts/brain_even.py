2bd4ebcef9e9ea7384164b981dbf80c54ab90f40
shlegeldavid <shlegeldavid@gmail.com>
2024-03-17 13:16:35 +0300

    Added logic for brain-even game

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,31 +1,9 @@
-import prompt
-import random
-
-
-def is_even(n):
-    if n % 2 == 0:
-        return 'yes'
-    return 'no'
-
+#!/usr/bin/env python3
+from ..even_game import *
 
 def main():
-    name = prompt.string('May I have your name? ')
-    print('Hello, ' + name)
-    print('Answer "yes" if the number is even, otherwise answer "no".')
-    n = 0
-    while n < 3:
-        random_number = random.randint(1, 100)
-        print('Question: ' + str(random_number))
-        answer = prompt.string('Your answer: ')
-        if is_even(random_number) == answer:
-            print('Correct!')
-            n += 1
-        else:
-            if answer == 'yes':
-                print(f"'{answer}' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, {name}!")
-            print(f"'{answer}' is wrong answer ;(. Correct answer was 'yes'.\nLet's try again, {name}!")
-            return
-    print(f'Congratulations, {name}!')
+    print('Welcome to the Brain Games!')
+    even_brain_game()
 
 
 if __name__ == '__main__':
