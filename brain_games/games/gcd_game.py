ab8fae21d92fc5603f77a121d7d86ed2d096b967
shlegeldavid <shlegeldavid@gmail.com>
2024-03-21 17:55:45 +0300

    Step 7 fully completed, gcd game is done

---
+++ brain_games/games/gcd_game.py
@@ -0,0 +1,30 @@
+from .engine import gcd_func, welcome_user, game_rule, question, answer_check, answer_request_integer, random_number_generation, congratulations
+
+
+def gcd_brain_game():
+    usernames = []
+    answers = []
+    correct_answers = []
+    numbers = []
+    gcd_rule = 'Find the greatest common divisor of given numbers.'
+
+    welcome_user(usernames)
+    game_rule(gcd_rule)
+    n = 0
+    while n < 3:
+        answers = []
+        random_number_generation(numbers)
+        random_number_generation(numbers)
+        first_number = numbers.pop(0)
+        second_number = numbers.pop()
+        gcd_question = f'{str(first_number)} {str(second_number)}'
+        question(gcd_question)
+        gcd = gcd_func(first_number, second_number)
+        correct_answers.append(gcd)
+        answer_request_integer(answers)
+        answer_check(answers, correct_answers, usernames)
+        if usernames == []:
+            return
+        n += 1
+    congratulations(usernames)
+    return
---
+++ brain_games/scripts/brain_gcd.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+from ..games.gcd_game import gcd_brain_game
+
+
+def main():
+    gcd_brain_game()
+
+
+if __name__ == '__main__':
+    main()
